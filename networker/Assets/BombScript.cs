c7fc807b5f541c97c77d060fc73367c6a434b559
Michael Rehn <rehn.michael@outlook.com>
2021-01-03 14:12:50 +0100

    Add custom collision detection for bomb

--- networker/Assets/BombScript.cs
+++ networker/Assets/BombScript.cs
@@ -14,10 +14,15 @@ public class BombScript : MonoBehaviour
 
   private float spawnTime;
 
+  private Vector3 previousPosition;
+  private Rigidbody body;
+
   private void Start()
   {
     spawnTime = Time.time;
-    GetComponent<Rigidbody>().velocity = transform.rotation * Vector3.forward * speed;
+    body = GetComponent<Rigidbody>();
+    body.velocity = transform.rotation * Vector3.forward * speed;
+    previousPosition = body.position;
   }
 
   // Update is called once per frame
@@ -27,13 +32,30 @@ public class BombScript : MonoBehaviour
     {
       NetworkState.Destroy(gameObject);
     }
+
   }
-  void OnCollisionEnter(Collision collision)
+
+  private void FixedUpdate()
+  {
+    if (NetworkState.IsServer)
+    {
+      var delta = body.position - previousPosition;
+      var ray = new Ray(previousPosition, delta);
+      if (Physics.Raycast(ray, out var hit, delta.magnitude))
+      {
+        OnHit(hit.transform.gameObject);
+      }
+
+      previousPosition = body.position;
+    }
+  }
+
+  private void OnHit(GameObject otherGo)
   {
     if (NetworkState.IsServer)
     {
       NetworkState.Destroy(gameObject);
-      if (collision.gameObject.TryGetComponent<NetworkedBody>(out var nb))
+      if (otherGo.TryGetComponent<NetworkedBody>(out var nb))
       {
         NetworkState.Server.InvokeEvent(new KickEvent(
           nb.id,
